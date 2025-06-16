cc281c60b11954c1ae79582b6369bcd22ee45c14
tonyliu66 <108905241+tonyliu66@users.noreply.github.com>
2025-06-16 23:32:09 +0100

    modified:   src/pages/index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -52,6 +52,7 @@ export default function Home({ siteTitle }) {
   }
 
   const onGenerateClick = () => {
+    console.log("Start button pressed. started=", started);
     if (!started) {
       setShowCaptcha(true)
     } else {
